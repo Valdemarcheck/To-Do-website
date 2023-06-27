12fb129c13eeda325f414643fc2d750361ba1fe5
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-06-27 18:20:25 +0500

    Create PubSub module

---
+++ src/PubSub.js
@@ -0,0 +1,37 @@
+export const PubSub = (() => {
+  const NOT_PRESENT_IN_THE_ARRAY = -1;
+  const events = {};
+
+  function emit(event, param = null) {
+    if (events[event]) {
+      for (let func of events[event]) {
+        func(param);
+      }
+    } else {
+      alert(`There is no event with a name '${event}'`);
+    }
+  }
+
+  function on(event, func) {
+    if (events[event]) {
+      events[event].push(func);
+    } else {
+      events[event] = [func];
+    }
+  }
+
+  function off(event, func) {
+    if (events[event]) {
+      const indexOfGivenFunction = events[event].indexOf(func);
+      if (indexOfGivenFunction !== NOT_PRESENT_IN_THE_ARRAY) {
+        events[event].splice(indexOfGivenFunction, 1);
+      }
+    } else {
+      alert(
+        `There is either no such event (${event}) registered, or your function isn't present there`
+      );
+    }
+  }
+
+  return { events, emit, on, off };
+})();
