4eb1ad6137946bed70927b9854effadec6dcd39b
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-06-27 17:32:08 +0500

    Setup debug website mockup for list creation

--- src/hello-world.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-  const a = document.createTextNode("p");
-  a.textContent = "Hello world!";
-  document.body.appendChild(a);
-};
--- src/index.js
+++
@@ -1,2 +0,0 @@
-import run from "./hello-world";
-run();
