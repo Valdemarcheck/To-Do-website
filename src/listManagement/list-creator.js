2ae10eb5f01214115a5999fd3ee9beb558f6b3a0
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-07-08 13:42:09 +0500

    Refactor code to remove DefaultList class

--- src/listManagement/list-creator.js
+++ src/listManagement/list-creator.js
@@ -1,12 +1,11 @@
 import { PubSub } from "../PubSub";
-import { DefaultList } from "./default-list";
-import { List } from "./list";
+import { List, addRemoveAndEditButtons } from "./list";
 
 export const DEFAULT_LIST_ID = "DEFAULT";
 
 function createDefaultList() {
   const creationData = { name: "Default", color: "#ccc" };
-  const defaultList = new DefaultList(creationData);
+  const defaultList = new List(creationData);
   defaultList.id = DEFAULT_LIST_ID;
   const listData = { list: defaultList, listId: defaultList.id };
   PubSub.emit("DefaultListPending", listData);
@@ -14,6 +13,7 @@ function createDefaultList() {
 
 function createNewList(newData) {
   const list = new List(newData);
+  addRemoveAndEditButtons(list);
   PubSub.emit("ListPending", list);
 }
 
--- src/listManagement/list.js
+++ src/listManagement/list.js
@@ -1,24 +1,86 @@
 import { PubSub } from "../PubSub";
 import { FORM_REGISTRY } from "../form-manager";
-import { DefaultList } from "./default-list";
+import { TaskCreator } from "../taskManagement/task-creator";
+import { TaskRegistrar } from "../taskManagement/task-registrar";
+import { TaskRenderer } from "../taskManagement/task-renderer";
 
-export class List extends DefaultList {
+export class List {
+  id = null;
+  div = null;
+
   constructor(data) {
-    super(data);
-    this.EditListButton = document.createElement("button");
-    this.EditListButton.addEventListener("click", () => {
-      PubSub.emit("UserWantsToEditList", this);
-      PubSub.emit("OpenForm", FORM_REGISTRY.list);
-    });
-    this.EditListButton.textContent = "edit";
+    this.name = data.name || "Unnamed";
+    this.color = data.color;
 
-    this.RemoveListButton = document.createElement("button");
-    this.RemoveListButton.addEventListener("click", () => {
-      PubSub.emit("ListShouldBeRemoved", this);
-    });
-    this.RemoveListButton.textContent = "x";
+    this.SortListButton = document.createElement("button");
+    this.SortListButton.textContent = "sort";
 
-    this.buttons.RemoveListButton = this.RemoveListButton;
-    this.buttons.EditListButton = this.EditListButton;
+    this.AddTaskButton = document.createElement("button");
+    this.AddTaskButton.addEventListener("click", () => {
+      PubSub.emit("OpenForm", FORM_REGISTRY.task);
+      PubSub.emit("ListIdGetsReturned", this.id);
+    });
+    this.AddTaskButton.textContent = "+";
+
+    this.buttons = {
+      SortListButton: this.SortListButton,
+      AddTaskButton: this.AddTaskButton,
+    };
+
+    this.setupTaskHelpers();
+    PubSub.on("TaskIsReadyForCreation", this.establishNewTask.bind(this));
+    PubSub.on("TaskIsReadyForEditing", this.editTask.bind(this));
+    PubSub.on("UserWantsToDeleteTask", this.deleteTask.bind(this));
   }
+
+  setupTaskHelpers() {
+    this.taskCreator = new TaskCreator();
+    this.taskRegistrar = new TaskRegistrar();
+    this.taskRenderer = new TaskRenderer(this.div);
+  }
+
+  establishNewTask(taskData) {
+    if (this.taskBelongsToThisList(taskData.parentList, this.id)) {
+      const task = this.taskCreator.createTask(taskData);
+      this.taskRegistrar.registerTask(task);
+      this.taskRenderer.renderTask(this.div, task);
+    }
+  }
+
+  editTask(taskData) {
+    if (this.taskBelongsToThisList(taskData.path.listId, this.id)) {
+      const editedTask = this.taskRegistrar.editTask(taskData);
+      this.taskRenderer.rerenderTask(this.div, editedTask);
+    }
+  }
+
+  deleteTask(task) {
+    console.log(task.parentList, this.id);
+    if (this.taskBelongsToThisList(task.parentList, this.id)) {
+      this.taskRegistrar.deleteTask(task);
+      this.taskRenderer.stopRenderingTask(task);
+    }
+  }
+
+  taskBelongsToThisList(listNameTaskIsLookingFor, currentListName) {
+    return listNameTaskIsLookingFor == currentListName;
+  }
+}
+
+export function addRemoveAndEditButtons(list) {
+  list.EditListButton = document.createElement("button");
+  list.EditListButton.addEventListener("click", () => {
+    PubSub.emit("UserWantsToEditList", list);
+    PubSub.emit("OpenForm", FORM_REGISTRY.list);
+  });
+  list.EditListButton.textContent = "edit";
+
+  list.RemoveListButton = document.createElement("button");
+  list.RemoveListButton.addEventListener("click", () => {
+    PubSub.emit("ListShouldBeRemoved", list);
+  });
+  list.RemoveListButton.textContent = "x";
+
+  list.buttons.RemoveListButton = list.RemoveListButton;
+  list.buttons.EditListButton = list.EditListButton;
 }
